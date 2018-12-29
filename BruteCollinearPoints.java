346f6ab56ba826123f8a10648c5ff76ca1f3e5bb
Eldar Kalachev <eldarkalachev@Eldars-MBP.Dlink>
2018-12-29 09:51:03 +0200

    Finished BruteCollinearPoints (need to be tested)

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,92 +1,92 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
+/******************************************************************************
+ *  Compilation:  javac BruteCollinearPoints.java
+ *  Execution:    java BruteCollinearPoints
+ *  Dependencies: LineSegment.java, Point.java
+ *
+ *  Examines 4 points at a time and checks whether they all lie on the same line
+ *  segment, returning all such line segments. To check whether the 4 points
+ *  p, q, r, and s are collinear, check whether the three slopes between
+ *  p and q, between p and r, and between p and s are all equal.
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BruteCollinearPoints {
-    private final LineSegment[] segments;
-    private int segmentsLength = 0;
+    private LineSegment[] segments;
 
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
-        if (points == null || points.length < 4)
-            throw new IllegalArgumentException("Array of points should contain 4 or more points");
+        if (points == null)
+            throw new IllegalArgumentException("Array of points is null");
 
-        sort(points);
+        checkDuplicated(points);
 
-        segments = new LineSegment[0];
+        Arrays.sort(points);
+
+        ArrayList<LineSegment> found = new ArrayList<LineSegment>();
 
         for (int p1 = 0; p1 < points.length - 3; p1++) {
             for (int p2 = p1 + 1; p2 < points.length - 2; p2++) {
                 for (int p3 = p2 + 1; p3 < points.length - 1; p3++) {
                     for (int p4 = p3 + 1; p4 < points.length; p4++) {
-                        Point point1 = points[p1];
-                        Point point2 = points[p2];
-                        Point point3 = points[p3];
-                        Point point4 = points[p4];
-
-                        double sloopP1P2 = point1.slopeTo(point2);
-                        double sloopP1P3 = point1.slopeTo(point2);
-                        double sloopP1P4 = point1.slopeTo(point4);
+                        double sloopP1P2 = points[p1].slopeTo(points[p2]);
+                        double sloopP1P3 = points[p1].slopeTo(points[p3]);
+                        double sloopP1P4 = points[p1].slopeTo(points[p4]);
 
                         if (sloopP1P2 == sloopP1P3 && sloopP1P2 == sloopP1P4) {
-                            //TODO add to array of segments
-                            new LineSegment(point1, point4);
+                            found.add(new LineSegment(points[p1], points[p4]));
                         }
                     }
                 }
             }
         }
+
+        segments = found.toArray(new LineSegment[0]);
     }
 
-    private static void merge(Point[] a, Point[] aux, int lo, int mid, int hi) {
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid)
-                aux[k] = a[j++];
-            else if (j > hi)
-                aux[k] = a[i++];
-            else if (less(a[j], a[i]))
-                aux[k] = a[j++];
-            else
-                aux[k] = a[i++];
+    private void checkDuplicated(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException("Sequence contains duplicates");
+                }
+            }
         }
-    }
-
-    private static boolean less(Point p1, Point p2) {
-        if (p1 == null || p2 == null)
-            throw new IllegalArgumentException();
-
-        return p1.compareTo(p2) < 0;
-    }
-
-    private static void sort(Point[] a, Point[] aux, int lo, int hi) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-
-        sort(aux, a, lo, mid);
-        sort(aux, a, mid + 1, hi);
-
-        merge(a, aux, lo, mid, hi);
-    }
-
-    private static void sort(Point[] a) {
-        Point[] aux = new Point[a.length];
-        sort(a, aux, 0, a.length - 1);
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return segmentsLength;
+        return segments.length;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return segments;
+        return Arrays.copyOf(segments, segments.length);
     }
 
     public static void main(String[] args) {
+        Point[] points = new Point[] {
+                new Point(1, 1),
+                new Point(2, 4),
+                new Point(4, 2),
+                new Point(43, 70),
+                new Point(3, 3),
+                new Point(1, 5),
+                new Point(6, 30),
+                new Point(2, 2),
+                new Point(4, 4),
+                new Point(4, 7),
+                new Point(7, 20)
+        };
 
+        BruteCollinearPoints bcp = new BruteCollinearPoints(points);
+
+        for (LineSegment ls : bcp.segments()) {
+            StdOut.println(ls.toString());
+        }
     }
 }
